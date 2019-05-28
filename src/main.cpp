4cdbfeede80c25d41cdaf774fcee003520ce59c6
Luis Alfredo <46092585+Zidrewndacht@users.noreply.github.com>
2019-05-28 16:06:22 -0300

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,10 @@ Pinos USB:      PA11 e PA12 - Evitar.
 Pinos 5V:		PB10, PB11, PB12, PB13, PB14, PB15, PA8, PA9, PA10, PA11, PA12, PA15, PB3, PB4, PB5, PB6, PB7, PB8, PB9, PB2 (Boot1), PA13 (SWDIO), PA14 (SWCLK)
 Pinos PWM: 		PB1, PB0, PA7, PA6, PA3, PA2, PA1, PA0, PA8, PA9, PA11, PB6, PB7, PB8, PB9
 Pinos TX/RX:	PA2/PA3, PB10/PB11, PA9/PA8, PB6/PB7
-Pinos ADC:      PB1, PB0, PA7, PA6, PA5, PA4, PA3, PA2, PA1, PA0    (todos 3V3 máx)
+Pinos ADC:      xPB1, xPB0, xPA7, xPA6, PA5, PA4, PA3, PA2, PA1, PA0    (todos 3V3 máx)
 */
 
-// Pinos Sensores utilizados para identificar a linha
+// Pinos Sensores utilizados para identificar a linha 
 #define S_ESQ2      PB10
 #define S_ESQ1      PA5
 #define S_CENTRO    PA4
@@ -15,62 +15,72 @@ Pinos ADC:      PB1, PB0, PA7, PA6, PA5, PA4, PA3, PA2, PA1, PA0    (todos 3V3 m
 #define S_DIR2      PB15
 #define S_VERDE_ESQ PA15
 #define S_VERDE_DIR PB3
+
 // Pinos Motores...
 #define M_RODA_DIR_HOR PB8
 #define M_RODA_DIR_ANT PB9
 #define M_RODA_ESQ_HOR PA0
 #define M_RODA_ESQ_ANT PA1
-#define M_SERVO1  PB0      // <===========================================    A definir ! ========================================================>
-#define M_SERVO2  PB1      // Suprimir caso se opte por atuador RC Servo.
-// Pinos encoders (requerem timers): https://github.com/chrisalbertson/quadratureBluePill/blob/master/examples/quadratureTest01/quadratureTest01.ino
-#define DT1     PA8
-#define CLK1    PA9
-#define DT2     PA6
-#define CLK2    PA7
+#define M_SERVO1  PB0
+#define M_SERVO2  PB1
+
 // Pinos ultrassom:
 #define TRIG     PB12   //saída
 #define ECHO     PB13   //entrada - precisa ser tolerante a 5V!
-// Pinos display i2c:
-// #define SDA PB7 
-// #define SCL PB6
-// Pinos Serial HC-05:
-// #define TX  PA2
-// #define RX  PA3
 
-// Estados da máquina de estados finitos:
-#define VERIFICA  0   //Verifica sensores
-#define LINHA     1 
-#define ESQ90     2
-#define DIR90     3   
-#define RET180    4   //Faz a volta e retorna
-#define CONTORNA  5   //Contorna obstáculo
+char sprintf_buffer[255];
 
-#define RAMPA       6
-//Mais estados a definir posteriormente, para salvamento após a rampa.
+//Controle PID:
+int sd2, sd1, sc, se1, se2;
+float dist_obst;
+float erro_atual, erro_anterior;
+float kp = 25, kd = 25;            //constante proporcional, constante derivativa. Ajustar experimentalmente.
 
-int estado = 0;             //estado atual da máquina de estados finitos
-int dist_encruzilhada = 0;  //distância calculada pelo encoder.
-
-int verifica_acc(){
-    return 0; //retornam 0 se normal, 1 se entrou na rampa:
+float calculaPID(){
+  float p = erro_atual;
+  //float i = i + error;   //componente integral não será usada.
+  float d = erro_atual-erro_anterior;
+  erro_anterior = erro_atual;   //d já calculado, então isso interfere apenas na iteração futura.
+  return (kp*p) /*+ (ki*i)*/ + (kd*d);
 }
 
-int verifica_obst(){
-    return 0; //retornam 0 se normal, 1 se enncontrou caixa
+// Funções de acesso aos motores:
+
+int verifica_obst(){    
+    //http://stm32duino.com/viewtopic.php?t=106
+    digitalWrite(TRIG,HIGH);
+    delay(1);
+    digitalWrite(TRIG,LOW);
+
+    dist_obst  = pulseIn(ECHO,HIGH) / 58.138f;
 }
 
-void verifica_verdes(){
-    if ( dist_encruzilhada > 100 ) {    //verifica verdes apenas se não acabou de sair de encruzilhada.
+void linha(){
+    sd2 = digitalRead(S_DIR2);
+    sd1 = digitalRead(S_DIR1);
+    sc =  digitalRead(S_CENTRO);
+    se1 = digitalRead(S_ESQ1);
+    se2 = digitalRead(S_ESQ2);
 
-        int verde_esq = digitalRead(S_VERDE_ESQ);
-        int verde_dir = digitalRead(S_VERDE_DIR);
+    //Cobrir gap: continua em linha reta se não há sensor lido;
+    /* Proposta de lógica para corrigir desalinhamento */
+    // Usar PID? Sugestão para controle via PID, menos compacta que a versão apresentada aqui: 
+    // https://www.instructables.com/id/Line-Follower-Robot-PID-Control-Android-Setup/
+    if      ( se2 && !se1 && !sc && !sd1 && !sd2) erro_atual = -4; //10000
+    else if ( se2 &&  se1 && !sc && !sd1 && !sd2) erro_atual = -3; //11000
+    else if (!se2 &&  se1 && !sc && !sd1 && !sd2) erro_atual = -2; //01000
+    else if (!se2 &&  se1 &&  sc && !sd1 && !sd2) erro_atual = -1; //01100
+    else if (!se2 && !se1 &&  sc && !sd1 && !sd2) erro_atual =  0; //00100
+    else if (!se2 && !se1 &&  sc &&  sd1 && !sd2) erro_atual =  1; //00110
+    else if (!se2 && !se1 && !sc &&  sd1 && !sd2) erro_atual =  2; //00010
+    else if (!se2 && !se1 && !sc &&  sd1 &&  sd2) erro_atual =  3; //00011
+    else if (!se2 && !se1 && !sc && !sd1 &&  sd2) erro_atual =  4; //00001
+    else erro_atual =  0; //sem linha ou leitura anormal, continue para frente
+    //if (!se2 && !se1 && !sc && !sd1 && !sd2) erro_atual = 0;   //sem linha, continue para frente. Movido acima
 
-        if      ( verde_esq == 1 && verde_dir == 1 ) estado = RET180;
-        else if ( verde_esq == 1 && verde_dir == 0 ) estado = ESQ90;
-        else if ( verde_esq == 0 && verde_dir == 1 ) estado = DIR90;
-        else    estado = LINHA;
-    } else  estado = LINHA;
 }
+
+// Geral
 
 void setup() {
     pinMode(S_DIR1,     INPUT);
@@ -83,44 +93,22 @@ void setup() {
     pinMode(M_RODA_ESQ_HOR,  OUTPUT);
     pinMode(M_RODA_DIR_ANT,  OUTPUT);
     pinMode(M_RODA_ESQ_ANT,  OUTPUT);
-    pinMode(M_SERVO1,    OUTPUT);
-    pinMode(M_SERVO2,    OUTPUT);
     
-    pinMode(TRIG, INPUT);
+    pinMode(TRIG, OUTPUT);
     pinMode(ECHO, INPUT);
 
     pinMode(PC13, OUTPUT);  //LED integrado
     Serial.begin(115200);
+    Serial.print(F("Setup OK\n\n"));
 }
 
 void loop() {
-    switch (estado){
-        case VERIFICA:
-            if          (verifica_acc())  estado = RAMPA;
-            else if     (verifica_obst()) estado = CONTORNA;
-            else        verifica_verdes();
-            break;
-        case LINHA:
-            /* incluir aqui lógica para corrigir desalinhamento*/
-
-            while ( digitalRead(S_ESQ2) && digitalRead(S_DIR2) ){
-                //Reseta trajeto lido pelo encoder e continua em linha reta até terminar encruzilhada
-                dist_encruzilhada = 0;
-                analogWrite(M_RODA_DIR_HOR, 128);
-                analogWrite(M_RODA_DIR_HOR, 128);
-            };
-            estado = VERIFICA; break;
-        case ESQ90:
-            
-            estado = VERIFICA; break;
-        case DIR90:
-            
-            estado = VERIFICA; break;
-        case RET180:
-            
-            estado = VERIFICA; break;
-        case RAMPA:
-            
-            break;
+    static int lastmillis;
+    verifica_obst();
+    linha();
+    if (millis() - lastmillis > 250) {
+        sprintf(sprintf_buffer,"Leitura: %d %d %d %d %d\tErro Atual/Anterior: %f/%f\n Obstáculo a: %f cm\n\n",se2, se1, sc, sd1, sd2, erro_atual, erro_anterior, dist_obst);
+        Serial.print(sprintf_buffer);
+        lastmillis = millis();
     }
 }
